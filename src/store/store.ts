7c1aecc3553371e44215259688516daaf778a666
MaheshaDinu98 <maheeshadinushan@gmail.com>
2025-08-08 22:46:09 +0530

    added user reducer to store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
 import bookReducer from "../slices/bookSlice.ts";
 import authReducer from "../slices/authSlice.ts";
+import userReducer from "../slices/userSlice.ts";
 
 export const store = configureStore({
     reducer:{
         books: bookReducer,
-        auth: authReducer
+        auth: authReducer,
+        users:userReducer
     }
 })
 
